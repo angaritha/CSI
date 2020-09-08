cb536f79de302105d475fffa6254c7641c87cffb
DilanGMB <Dilan.baron79@gmail.com>
2020-09-08 16:16:18 -0500

    Commit test CSI

---
+++ ingSistemas.java
@@ -0,0 +1,9 @@
+public class ingSistemas {
+
+  public static void main(String[] args) {
+    System.out.println("      Ingenieria de Sistemas");
+    System.out.println("              01D67");
+    System.out.println("           CSI - Miami");
+    System.out.println("Configuración de Sistemas Informaticos");
+  }
+}
